8691f5789c2ccdb02b4762058c4d5329f604ae99
Yassine CHALH <yassinechalh9@gmail.com>
2023-08-03 03:05:21 +0100

    TypeScript configuration

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-"use strict";
+
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var users_1 = require("../controllers/users");
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var body_parser_1 = require("body-parser");
